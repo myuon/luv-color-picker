7ddfd7081c21fa1f8d967f5f94aff9e451829088
myuon <ioi.joi.koi.loi@gmail.com>
2020-02-01 17:28:06 +0900

    feat: color circle

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,23 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react-hooks", "sort-imports-es6-autofix"],
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended"
+  ],
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      rules: {
+        "no-unused-vars": 0,
+        "@typescript-eslint/no-unused-vars": [2, { args: "none" }],
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "@typescript-eslint/explicit-function-return-type": "off"
+      }
+    }
   ]
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,81 @@
-import React from "react";
+import React, { useRef, useEffect, useCallback, useState } from "react";
+import { hsluvToRgb } from "hsluv";
+
+const range = (n: number) => Array.from({ length: n }, (v, k) => k);
+
+const vecToArg = (xy: [number, number]) => {
+  const [x, y] = xy;
+  const h = (Math.atan2(y, x) * 180) / Math.PI;
+  return (360 + h) % 360;
+};
+
+const vecSize = (xy: [number, number]) =>
+  Math.sqrt(xy[0] * xy[0] + xy[1] * xy[1]);
+
+const useForceUpdate = () => {
+  const [, setState] = useState();
+  return setState;
+};
 
 export const App: React.FC = () => {
-  return <>foo</>;
+  const [light, setLight] = useState(50);
+  const forceUpdate = useForceUpdate();
+
+  const canvasRef = useRef(null);
+  const getContext = (): CanvasRenderingContext2D => {
+    const canvas = canvasRef.current as any;
+
+    return canvas.getContext("2d");
+  };
+  const width = 300;
+  const height = 300;
+
+  useEffect(() => {
+    const ctx = getContext();
+
+    // x: hue
+    // y: saturation
+    range(width).forEach(x => {
+      range(height).forEach(y => {
+        const pv: [number, number] = [
+          x / (width / 2) - 1,
+          -y / (height / 2) + 1
+        ];
+        const pvSize = vecSize(pv);
+        if (pvSize <= 1.0) {
+          const h = vecToArg(pv);
+          const [r, g, b] = hsluvToRgb([h, pvSize * 100, light]);
+
+          ctx.fillStyle = `rgb(${r * 255}, ${g * 255}, ${b * 255})`;
+          ctx.fillRect(x, y, 1, 1);
+        }
+      });
+    });
+
+    ctx.save();
+  }, [light]);
+
+  const handleChange = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const form = new FormData(event.currentTarget);
+      const input = Object.fromEntries(form);
+      setLight(parseInt(input["light"] as string, 10));
+      forceUpdate(true);
+    },
+    [forceUpdate]
+  );
+
+  return (
+    <>
+      <h1>LUV Color Picker</h1>
+      <canvas width={width} height={height} ref={canvasRef} />
+
+      <form onChange={handleChange}>
+        <label htmlFor="light">Light</label>
+        <input type="number" name="light" max={100} min={0} defaultValue={50} />
+      </form>
+    </>
+  );
 };
